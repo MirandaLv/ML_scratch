f19a1fa5f17ad938a28030cec6c39320e888a59c
MirandaLv <zhonghui.lv@gmail.com>
2025-04-16 22:19:08 -0400

    add new func

---
+++ scripts/classification_report.py
@@ -0,0 +1,47 @@
+
+import numpy as np
+
+def classification_report(y_true, y_pred, labels=None):
+    # for cases that in y_true missing certain labels
+    if labels==None:
+        labels = sorted(set(y_true + y_pred))
+
+    report = {}
+    for label in labels:
+        tp = sum((yt == label and yp == label) for yt, yp in zip(y_true, y_pred))
+        fp = sum((yt != label and yp == label) for yt, yp in zip(y_true, y_pred))
+        fn = sum((yt == label and yp != label) for yt, yp in zip(y_true, y_pred))
+
+        precision = tp / (tp + fp) if tp + fp > 0 else 0.0
+        recall = tp / (tp + fn) if tp + fn > 0 else 0.0
+        f1 = 2 * precision * recall / (precision + recall) if precision + recall > 0 else 0.0
+        case_sum_support = sum(yt == label for yt in y_true)
+
+
+        report[label] = {"precision": precision,
+                         "recall": recall,
+                         "f1": f1,
+                         "support": case_sum_support}
+
+    overall_recall = sum(v["recall"] for v in report.values()) / len(labels)
+    overall_precision = sum(v["precision"] for v in report.values()) / len(labels)
+    overall_f1 = sum(v["f1"] for v in report.values()) / len(labels)
+    overall_support = sum(v["support"] for v in report.values())
+
+    report["avg / total"] = {
+        "precision": round(overall_precision, 2),
+        "recall": round(overall_recall, 2),
+        "f1-score": round(overall_f1, 2),
+        "support": overall_support
+    }
+
+    return report
+
+
+# Example usage
+y_true = [1, 2, 2, 4, 5, 1, 2, 3, 4, 5]
+y_pred = [1, 2, 2, 4, 5, 1, 3, 3, 4, 5]
+
+report = classification_report(y_true, y_pred)
+for label, metrics in report.items():
+    print(f"{label}: {metrics}")
---
+++ scripts/mean_median_mode.py
@@ -0,0 +1,48 @@
+
+import numpy as np
+
+def calc_median(nums):
+    """Calculate the median value of a list"""
+    if not nums: return None
+
+    sorted_nums = sorted(nums)
+    n = len(sorted_nums)
+
+    if n % 2 == 0: # even number of list
+        mid = n // 2
+        prev = mid - 1
+        median_val = (sorted_nums[mid] + sorted_nums[prev]) / 2
+    else:
+        mid = n // 2
+        median_val = sorted_nums[mid]
+
+    return median_val
+
+def calc_mode(nums):
+    """Calculate the mode value of a list"""
+
+    if not nums: return None # edge case
+
+    hashmap = {}
+    for num in nums:
+        hashmap[num] = hashmap.get(num, 0) + 1
+
+    max_freq = max(hashmap.values())
+
+    res = [] # it could be multiple result
+    for k, v in hashmap.items():
+        if v == max_freq:
+            res.append(k)
+
+    return res
+
+
+def calc_mean(nums):
+    if not nums: return None
+    return np.array(nums).mean()
+
+example = [1,2,3,3,2,5,6,7,8]
+example1 = []
+print(calc_median(example))
+print(calc_mode(example))
+print(calc_mean(example))
